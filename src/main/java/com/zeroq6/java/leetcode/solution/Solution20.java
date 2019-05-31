5205021a40bfdc970ca641d2fdea2452ddaf6616
icgeass <icgeass@hotmail.com>
2019-05-31 17:35:09 +0800

    leecode问题20,用栈判断括号

---
+++ src/main/java/com/zeroq6/java/leetcode/solution/Solution20.java
@@ -0,0 +1,70 @@
+package com.zeroq6.java.leetcode.solution;
+
+
+/**
+ * https://leetcode-cn.com/problems/valid-parentheses/
+ * <p>
+ * <p>
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+ * <p>
+ * 有效字符串需满足：
+ * <p>
+ * 左括号必须用相同类型的右括号闭合。
+ * 左括号必须以正确的顺序闭合。
+ * 注意空字符串可被认为是有效字符串。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: "()"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: "()[]{}"
+ * 输出: true
+ * 示例 3:
+ * <p>
+ * 输入: "(]"
+ * 输出: false
+ * 示例 4:
+ * <p>
+ * 输入: "([)]"
+ * 输出: false
+ * 示例 5:
+ * <p>
+ * 输入: "{[]}"
+ * 输出: true
+ */
+public class Solution20 {
+
+    public boolean isValid(String s) {
+
+        StringBuilder stringBuilder = new StringBuilder();
+
+        int len = s.length();
+        for (int i = 0; i < len; i++) {
+            char ch = s.charAt(i);
+            stringBuilder.append(ch);
+            int stringBuilderLen = stringBuilder.length();
+            if (stringBuilderLen > 1) {
+                char matchTo = stringBuilder.charAt(stringBuilderLen - 2);
+                if (matchTo == '[' && ch == ']' || matchTo == '{' && ch == '}' || matchTo == '(' && ch == ')') {
+                    stringBuilder.delete(stringBuilderLen - 2, stringBuilderLen);
+                }
+            }
+        }
+
+        return stringBuilder.length() == 0;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution20().isValid("()"));
+        System.out.println(new Solution20().isValid("{}()[]"));
+        System.out.println(new Solution20().isValid("(]"));
+        System.out.println(new Solution20().isValid("([)]"));
+        System.out.println(new Solution20().isValid("{[]}"));
+
+
+
+    }
+}
