9d1b010f509eff53c92aff2acbd6b50020269229
icgeass <icgeass@hotmail.com>
2019-05-31 16:59:04 +0800

    leecode14,查找最长共同前缀

---
+++ src/main/java/com/zeroq6/java/leetcode/solution/Solution14.java
@@ -0,0 +1,59 @@
+package com.zeroq6.java.leetcode.solution;
+
+
+/**
+ * https://leetcode-cn.com/problems/longest-common-prefix/
+ * <p>
+ * 编写一个函数来查找字符串数组中的最长公共前缀。
+ * <p>
+ * 如果不存在公共前缀，返回空字符串 ""。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: ["flower","flow","flight"]
+ * 输出: "fl"
+ * 示例 2:
+ * <p>
+ * 输入: ["dog","racecar","car"]
+ * 输出: ""
+ * 解释: 输入不存在公共前缀。
+ * 说明:
+ * <p>
+ * 所有输入只包含小写字母 a-z 。
+ */
+public class Solution14 {
+
+    public String longestCommonPrefix(String[] strs) {
+        String result = "";
+        int len = strs[0].length();  // 字符串数组中最小长度字符串长度
+        // 查找最小长度
+        for (int i = 1; i < strs.length; i++) {
+            int length = strs[i].length();
+            len = len > length ? length : len;
+        }
+        // 查找相同
+        for (int i = 0; i < len; i++) {
+            char ch = strs[0].charAt(i);
+            boolean flag = true;
+            for (int j = 1; j < strs.length; j++) {
+                if(ch != strs[j].charAt(i)){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                result = result.concat(ch + "");
+            }
+        }
+        return result;
+
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(new Solution14().longestCommonPrefix(new String[]{"flower","flow","flight"}));
+
+        System.out.println(new Solution14().longestCommonPrefix(new String[]{"dog","racecar","car"}));
+
+    }
+}
