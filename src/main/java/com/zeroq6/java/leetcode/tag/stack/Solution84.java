c1deeae916cbf981970f89236dcf33abde23f7ed
icgeass <icgeass@hotmail.com>
2019-08-21 15:47:22 +0800

    84. 柱状图中最大的矩形

---
+++ src/main/java/com/zeroq6/java/leetcode/tag/stack/Solution84.java
@@ -0,0 +1,64 @@
+package com.zeroq6.java.leetcode.tag.stack;
+
+import com.zeroq6.java.leetcode.solution.help.ArrayHelper;
+
+/**
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ * <p>
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ * <p>
+ *  
+ * <p>
+ * <p>
+ * <p>
+ * 以上是柱状图的示例，其中每个柱子的宽度为 1，给定的高度为 [2,1,5,6,2,3]。
+ * <p>
+ *  
+ * <p>
+ * <p>
+ * <p>
+ * 图中阴影部分为所能勾勒出的最大矩形面积，其面积为 10 个单位。
+ * <p>
+ *  
+ * <p>
+ * 示例:
+ * <p>
+ * 输入: [2,1,5,6,2,3]
+ * 输出: 10
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/largest-rectangle-in-histogram
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution84 {
+
+
+    public int largestRectangleArea(int[] heights) {
+        if (null == heights || heights.length == 0) {
+            return 0;
+        }
+        int max = 0;
+        // 从左向右扫描最大矩形面积
+        for (int i = 0; i < heights.length; i++) {
+            int minHeight = heights[i];
+            for (int j = i; j < heights.length; j++) {
+                if (heights[j] < minHeight) {
+                    minHeight = heights[j];
+                }
+                int areaSize = minHeight * (j - i + 1);
+                if (max < areaSize) {
+                    max = areaSize;
+                }
+            }
+        }
+        return max;
+
+
+    }
+
+
+    public static void main(String[] args) {
+        Solution84 solution84 = new Solution84();
+        System.out.println(solution84.largestRectangleArea(ArrayHelper.genIntArray(2, 1, 5, 6, 2, 3)));
+    }
+}
