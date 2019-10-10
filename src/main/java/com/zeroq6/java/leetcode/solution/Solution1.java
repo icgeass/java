bd4cc0f6a14bcb10bfbd9481951afa786c507228
icgeass <icgeass@hotmail.com>
2019-10-10 10:19:44 +0800

    leecode，题目1解法2，使用头尾指针查找

--- src/main/java/com/zeroq6/java/leetcode/solution/Solution1.java
+++ src/main/java/com/zeroq6/java/leetcode/solution/Solution1.java
@@ -2,6 +2,9 @@ package com.zeroq6.java.leetcode.solution;
 
 
 import com.alibaba.fastjson.JSON;
+import com.zeroq6.java.leetcode.solution.help.ArrayHelper;
+
+import java.util.Arrays;
 
 /**
  * https://leetcode-cn.com/problems/two-sum/
@@ -21,14 +24,52 @@ import com.alibaba.fastjson.JSON;
 public class Solution1 {
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
-        lab:
         for (int i = 0; i < nums.length - 1; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] == target) {
                     result[0] = i;
                     result[1] = j;
-                    break lab;
+                    return result;
                 }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 注意题目：你可以假设每种输入只会对应一个答案。
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSum1(int[] nums, int target) {
+        int[] numsCopy = Arrays.copyOf(nums, nums.length);
+        Arrays.sort(numsCopy);
+        int first = 0;
+        int last = numsCopy.length - 1;
+        int sum = numsCopy[first] + numsCopy[last];
+        int[] result = new int[]{-1, -1};
+        while (sum != target) {
+            if (sum > target) {
+                // sum大则尾指针前移，减小sum
+                last--;
+            } else {
+                // sum小则头指针后移，增大sum
+                first++;
+            }
+            // 不可能出现break了但是未找到的情况，因为题目指定只有一个结果
+            if (first >= last) {
+                break;
+            }
+            sum = numsCopy[first] + numsCopy[last];
+        }
+        // 查找原始位置
+        for (int i = 0; i < nums.length; i++) {
+            if (result[0] == -1 && nums[i] == numsCopy[first]) {
+                result[0] = i;
+            } else if (result[1] == -1 && nums[i] == numsCopy[last]) {
+                result[1] = i;
             }
         }
         return result;
@@ -36,7 +77,10 @@ public class Solution1 {
 
 
     public static void main(String[] args) {
-        Object result = new Solution1().twoSum(new int[]{1, 1, 5, 5, 6}, 10);
+        Object result = new Solution1().twoSum(ArrayHelper.genIntArray(2, 7, 11, 15), 9);
+        System.out.println(JSON.toJSONString(result));
+
+        result = new Solution1().twoSum1(ArrayHelper.genIntArray(2, 7, 11, 15), 9);
 
         System.out.println(JSON.toJSONString(result));
     }
