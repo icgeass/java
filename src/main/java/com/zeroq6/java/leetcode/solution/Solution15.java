8b8c80110ae3887c6f4c6b05beb0d3850030bfcc
icgeass <icgeass@hotmail.com>
2019-12-02 22:58:32 +0800

    三数之和，超出时间限制todo

---
+++ src/main/java/com/zeroq6/java/leetcode/solution/Solution15.java
@@ -0,0 +1,54 @@
+package com.zeroq6.java.leetcode.solution;
+
+import com.zeroq6.java.leetcode.solution.help.ArrayHelper;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * 给定一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？找出所有满足条件且不重复的三元组。
+ * <p>
+ * 注意：答案中不可以包含重复的三元组。
+ * <p>
+ * 例如, 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ * <p>
+ * 满足要求的三元组集合为：
+ * [
+ * [-1, 0, 1],
+ * [-1, -1, 2]
+ * ]
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution15 {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length && j != i; j++) {
+                for (int k = 0; k < nums.length && k != i && k != j; k++) {
+                    if (nums[i] + nums[j] + nums[k] == 0) {
+                        List<Integer> list = Arrays.asList(nums[i], nums[j], nums[k]);
+                        Collections.sort(list);
+                        if (!result.contains(list)) {
+                            result.add(list);
+                        }
+                    }
+                }
+            }
+        }
+        return result;
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution15().threeSum(ArrayHelper.genIntArray(-1, 0, 1, 2, -1, -4)));
+    }
+}
+
+
