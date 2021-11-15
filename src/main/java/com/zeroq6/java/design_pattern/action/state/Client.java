1986892b67bc10a205c1deba9a9c2972344b65c7
icgeass <icgeass@hotmail.com>
2021-11-16 00:43:31 +0800

    状态模式

--- src/main/java/com/zeroq6/java/design_pattern/action/state/Client.java
+++ src/main/java/com/zeroq6/java/design_pattern/action/state/Client.java
@@ -7,6 +7,11 @@ package com.zeroq6.java.design_pattern.action.state;
  *
  * Context被动通过状态改变来表现出不同的行为
  * 每个状态行为结束后,将Context的状态设置为下一个状态,然后调用Context让其表现出其他状态的行为
+ *
+ * Allow an object to alter its behavior when its internal state changes. The
+ * object will appear to change its class.
+ * 意思是：当一个对象内在状态改变时允许改变行为，这个对象看起
+ * 来像改变了其类型。
  */
 public class Client {
     public static void main(String[] args) {
